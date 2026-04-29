d3e6cbbff42b46387f11d99b67aaa7be8ff9583d
Bryan Andrews <andrewsb@uchicago.edu>
2026-04-29 11:48:12 -0500

    updated installation test

--- test_installation.sh
+++ test_installation.sh
@@ -3,10 +3,8 @@
 
 set -e  # Exit on error
 
-echo "Running bcar_sort..."
-./bcar_sort --in test/testing_reads.fastq --bc-start 0 --bc-len 15 --out test/test_sorted.fastq --temp .
-echo "Running bcar_merge..."
-./bcar_merge --in test/test_sorted.fastq --bc-start 0 --bc-len 15 --out test/test_output.fastq --threads 10
+echo "Running bcar"
+./bcar --in test/testing_reads.fastq --bc-start 0 --bc-len 15 --out test/test_output.fastq --tmp-dir .
 
 echo "Comparing output to target..."
 result=$(python3 test/compare_fastqs.py test/test_output.fastq test/target_output.fastq)
@@ -14,7 +12,6 @@ result=$(python3 test/compare_fastqs.py test/test_output.fastq test/target_outpu
 if [ "$result" = "True" ]; then
     echo "SUCCESS: Output matches target. bcar is functioning as expected."
     rm test/test_output.fastq
-    rm test/test_sorted.fastq
     exit 0
 else
     echo "FAILURE: Output does not match target. bcar output differs from expected."
